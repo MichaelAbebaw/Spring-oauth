8c4c642a854fa56cd43d8b7f07d046c7f5cd1e44
Michael <abebaw.michael@gmail.com>
2023-05-17 15:06:12 -0700

    Add files via upload

--- src/main/java/com/oauth2/springoauth/configuration/OauthConfiguration.java
+++ src/main/java/com/oauth2/springoauth/configuration/OauthConfiguration.java
@@ -2,6 +2,7 @@ package com.oauth2.springoauth.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -20,5 +21,17 @@ public class OauthConfiguration {
                 .authenticated().and()
                 .oauth2Login(Customizer.withDefaults())
                 .build();
+    }
+
+    @Bean
+    @Order(2)
+    public SecurityFilterChain configureOauthSecurity2 (HttpSecurity httpSecurity) throws Exception {
+        // with a lambda expression
+        return httpSecurity.authorizeHttpRequests(auth -> {
+            auth.requestMatchers(HttpMethod.GET, "/").permitAll();
+            auth.requestMatchers(HttpMethod.GET, "/secured").authenticated();
+        })
+        .oauth2Login(Customizer.withDefaults())
+        .build();
     }
 }
